a9561c5cf83f8ff97c83f17de6ab812db671bfa4
Ronan Connolly <Ronan@RonanConnolly.dev>
2024-05-03 18:32:28 +0200

    Remove default component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'mose-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'movie-search';
-}
+export class AppComponent {}
