063d9f82af1cbd5fd27d4e5ff9471ab11bd9a049
Ronan Connolly <Ronan@RonanConnolly.dev>
2024-05-03 20:16:50 +0200

    tidy and add in the httpclient

--- src/app/shared/data/rest/the-movie-db.service.ts
+++ src/app/shared/data/rest/the-movie-db.service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TheMovieDbService {}
+export class TheMovieDbService {
+  public constructor(private httpClient: HttpClient) {}
+
+  public getMovies() {
+    return this.httpClient.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}`
+    );
+  }
+}
